27b215c125752f8b72a0e50ecd197e15ec4d2dde
Administrateur <Administrateur@WINDOWS-OEBGAGP.ad.campus-eni.fr>
2016-07-20 11:58:00 +0200

    servlet gestionEmploye

--- src/Servlet/GestionEmploye.java
+++ src/Servlet/GestionEmploye.java
@@ -1,10 +1,15 @@
 package Servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import metier.Personnel;
 
 /**
  * Servlet implementation class GestionEmploye
@@ -25,6 +30,7 @@ public class GestionEmploye extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		valider(request,response);
 	}
 
 	/**
@@ -32,6 +38,44 @@ public class GestionEmploye extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		valider(request,response);
 	}
 
+	protected void valider(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+     	RequestDispatcher dispatcher;
+     	String ajouterParam = request.getParameter("bAjouter"); 
+		String modifParam = request.getParameter("bModifier");
+		String supprimeParam = request.getParameter("bSupprimer");
+		
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String login =  request.getParameter("login");
+		String mdp =  request.getParameter("mdp");
+		
+		Personnel unePersonne = new Personnel();
+		
+		if(ajouterParam != null)
+		{
+			unePersonne.setEnonce(enonce);
+			if(taRace != null) laQuestion.setMarque(1);
+			
+			try {
+				QuestionDAO.QuestionAjouter(laQuestion);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		if(modifParam != null)
+		{
+			
+		}		
+		
+		if(supprimeParam != null)
+		{
+			
+		}
+}
 }
