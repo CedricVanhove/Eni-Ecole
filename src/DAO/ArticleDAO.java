d3bff101029cf0f0e19c0bf67e81c9505db3424c
Administrateur <Administrateur@WINDOWS-H9USGLH.ad.campus-eni.fr>
2016-07-20 09:55:30 +0200

    ceci est un commentaire

--- src/DAO/ArticleDAO.java
+++ src/DAO/ArticleDAO.java
@@ -1,6 +1,7 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -29,7 +30,12 @@ public class ArticleDAO
 		   
 		 while(result.next())
 		 {
-			 lArticle.setNum(result.getInt("idArticle"));
+			 lArticle.setNum(result.getInt("idArticle")); // verif nom des colonnes
+			 lArticle.setLibelle(result.getString("libelle"));
+			 lArticle.setDescription(result.getString("description"));
+			 lArticle.setPoids(result.getString("poids"));
+			
+			 
 		 }
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
@@ -39,10 +45,83 @@ public class ArticleDAO
 	{
 		  result.close();
 		     state.close();
-	}
-
-   
+	}   
 	return lesArticles;
 	 
+ }
+ public boolean Insert(Article item)
+ {
+	 boolean izOkay = false;
+	 PreparedStatement state = null;
+	ResultSet result = null;
+	try
+	{
+		 Connection conn = AccesBase.getConnection();
+		 state = conn.prepareStatement( "INSERT INTO article (libelle, description, poids) VALUES(?, ?, ?);" );
+
+		state.setString( 1, item.getLibelle());
+		  state.setString( 2, item.getDescription());
+		  state.setString( 3, item.getPoids() );
+
+		 /* Exécution de la requête */
+		 int statut = state.executeUpdate();
+		 if(statut > 0 ) izOkay = true;
+	}
+	catch( Exception ex)
+	{
+		izOkay = false;
+	}
+	
+	 return izOkay;
+ }
+ public boolean Delete(int id)
+ {
+	 boolean izOkay = false;
+	 PreparedStatement state = null;
+	ResultSet result = null;
+	try
+	{
+		 Connection conn = AccesBase.getConnection();
+		 state = conn.prepareStatement( "Delete from article where idArticle = ?" );
+
+		state.setInt(  1, id);
+		 
+
+		 /* Exécution de la requête */
+		 int statut = state.executeUpdate();
+		 if(statut > 0 ) izOkay = true;
+	}
+	catch( Exception ex)
+	{
+		izOkay = false;
+	}
+	
+	 return izOkay;
+ }
+ public boolean Update(Article item)
+ {
+	 boolean izOkay = false;
+	 PreparedStatement state = null;
+	ResultSet result = null;
+	try
+	{
+		 Connection conn = AccesBase.getConnection();
+		 state = conn.prepareStatement( "Update article set libelle=?,description=?,poids=?" );
+
+		 state.setString( 1, item.getLibelle());
+		  state.setString( 2, item.getDescription());
+		  state.setString( 3, item.getPoids() );
+		 
+
+		 /* Exécution de la requête */
+		 int statut = state.executeUpdate();
+		 if(statut > 0 ) izOkay = true;
+	}
+	catch( Exception ex)
+	{
+		izOkay = false;
+	}
+	
+	 return izOkay;
  }
 }
--- src/DAO/CommandeDAO.java
+++ src/DAO/CommandeDAO.java
@@ -1,5 +1,5 @@
 package DAO;
 
 public class CommandeDAO {
-
+	// details commande lArticle.setQuantite(result.getString("quantite"));
 }
