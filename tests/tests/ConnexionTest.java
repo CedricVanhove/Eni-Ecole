0d2b306eca874ee436d1b8d0c916a751025edb39
achilla2016 <achilla2016@WINDOWS-ARVLPCA.ad.campus-eni.fr>
2016-07-20 16:35:56 +0200

    test unitaire

---
+++ tests/tests/ConnexionTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author achilla2016
+ *
+ */
+public class ConnexionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
