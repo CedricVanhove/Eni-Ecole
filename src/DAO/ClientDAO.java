6815c031d808e46aff99f1cdc87d90958faac2e2
achilla2016 <achilla2016@WINDOWS-ARVLPCA.ad.campus-eni.fr>
2016-07-20 11:40:39 +0200

    ajouts clientdao et modif bean client

--- src/DAO/ClientDAO.java
+++ src/DAO/ClientDAO.java
@@ -1,5 +1,112 @@
 package DAO;
 
-public class ClientDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import sun.text.CodePointIterator;
+import metier.Client;
+
+public class ClientDAO {
+	
+	public static void addClient(Client client) throws SQLException{
+		PreparedStatement rqt=null;
+		Connection cnx=null;
+		try{
+			cnx=AccesBase.getConnection();
+			rqt=cnx.prepareStatement("insert into client(nom, adresse, codepostal, ville, tel, mail) values (?,?,?,?,?)");
+			rqt.setString(1, client.getNom());
+			rqt.setString(2, client.getAdresse());
+			rqt.setString(3, client.getCP());
+			rqt.setString(4, client.getVille());
+			rqt.setString(5, client.getTel());
+			rqt.setString(6, client.getMail());
+			rqt.executeUpdate();
+		}finally{
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+	}
+	
+	public static Client getClientById(int idClient) throws SQLException{
+		Client client = new Client();
+		PreparedStatement rqt=null;
+		Connection cnx=null;
+		ResultSet rs=null;
+		try{
+			cnx=AccesBase.getConnection();
+			rqt=cnx.prepareStatement("select * from client where idclient = ?");
+			rqt.setInt(1, idClient);
+			rs=rqt.executeQuery();
+			while (rs.next()){
+				client.setId(idClient);
+				client.setNom(rs.getString("nom"));				
+				client.setAdresse(rs.getString("adresse"));
+				client.setCP(rs.getString("cp"));
+				client.setVille(rs.getString("ville"));
+				
+			}
+		}finally{
+			if (rs!=null) rs.close();
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		return client;
+	}
+
+	public static Client getClientByNom(String nom,String cp) throws SQLException{
+		Client client = new Client();
+		PreparedStatement rqt=null;
+		Connection cnx=null;
+		ResultSet rs=null;
+		try{
+			cnx=AccesBase.getConnection();
+			rqt=cnx.prepareStatement("select * from client where nom = ? and codePostal=?");
+			rqt.setString(1,nom);
+			rqt.setString(2, cp);
+			rs=rqt.executeQuery();
+			while (rs.next()){
+				client.setId(rs.getInt("idClient"));
+				client.setNom(nom);				
+				client.setAdresse(rs.getString("adresse"));
+				client.setCP(cp);
+				client.setVille(rs.getString("ville"));	
+			}
+		}finally{
+			if (rs!=null) rs.close();
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		return client;
+	}
+	
+	public static Client getClient() throws SQLException{
+		Client client = new Client();
+		PreparedStatement rqt=null;
+		Connection cnx=null;
+		ResultSet rs=null;
+		try{
+			cnx=AccesBase.getConnection();
+			rqt=cnx.prepareStatement("select * from client ");
+			rs=rqt.executeQuery();
+			while (rs.next()){
+				client.setId(rs.getInt("idClient"));
+				client.setNom(rs.getString("nom"));				
+				client.setAdresse(rs.getString("adresse"));
+				client.setCP(rs.getString("cp"));
+				client.setVille(rs.getString("ville"));	
+				client.setMail(rs.getString("mail"));
+			}
+		}finally{
+			if (rs!=null) rs.close();
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+		return client;
+	}
+
+	
+	
+	
 }
--- src/metier/Client.java
+++ src/metier/Client.java
@@ -4,7 +4,6 @@ public class Client
 {
  private int id;
  private String nom;
- private String prenom;
  private String adresse;
  private String CP;
  private String ville;
@@ -22,12 +21,7 @@ public String getNom() {
 public void setNom(String nom) {
 	this.nom = nom;
 }
-public String getPrenom() {
-	return prenom;
-}
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
+
 public String getAdresse() {
 	return adresse;
 }
@@ -60,16 +54,14 @@ public void setMail(String mail) {
 }
 @Override
 public String toString() {
-	return "Client [id=" + id + ", nom=" + nom + ", prenom=" + prenom
-			+ ", adresse=" + adresse + ", CP=" + CP + ", ville=" + ville
+	return "Client [id=" + id + ", nom=" + nom + ", adresse=" + adresse + ", CP=" + CP + ", ville=" + ville
 			+ ", tel=" + tel + ", mail=" + mail + "]";
 }
-public Client(int id, String nom, String prenom, String adresse, String cP,
+public Client(int id, String nom, String adresse, String cP,
 		String ville, String tel, String mail) {
 	super();
 	this.id = id;
 	this.nom = nom;
-	this.prenom = prenom;
 	this.adresse = adresse;
 	CP = cP;
 	this.ville = ville;
