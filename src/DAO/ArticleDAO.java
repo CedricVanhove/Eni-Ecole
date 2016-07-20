94e6a8a0c9fa43798279e0cada7dff72df7d2c97
Administrateur <Administrateur@WINDOWS-H9USGLH.ad.campus-eni.fr>
2016-07-20 12:15:52 +0200

    ta daronne -1

--- src/DAO/ArticleDAO.java
+++ src/DAO/ArticleDAO.java
@@ -19,7 +19,7 @@ public class ArticleDAO
 	Statement state = null;
 	ResultSet result = null;
 	try {
-		lesArticles = new ArrayList<Article>();
+		
 		 Article lArticle = new Article();
 		 
 		 Connection conn = AccesBase.getConnection();
