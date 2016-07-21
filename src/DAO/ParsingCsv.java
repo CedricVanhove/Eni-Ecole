2c38f9a73ff55dfd7258535c2bce0f9f89f340d8
achilla2016 <achilla2016@WINDOWS-ARVLPCA.ad.campus-eni.fr>
2016-07-21 11:45:07 +0200

    ajout script import

---
+++ src/DAO/ParsingCsv.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package DAO;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.opencsv.CSVReader;
+
+
+
+/**
+ * @author achilla2016
+ *
+ */
+public class ParsingCsv {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CSVReader reader = null;
+		try 
+		{
+			//Get the CSVReader instance with specifying the delimiter to be used
+			reader = new CSVReader(new FileReader("d:\\csvexpeditor.csv"),';');
+			String [] nextLine;
+			//Read one line at a time
+		    while ((nextLine = reader.readNext()) != null) 
+		    {
+		    	for(String token : nextLine)
+				{if(token.contains(";")){
+					String []mat=token.split(";");
+					for (String item : mat) {
+						//System.out.println(item);
+					}
+				}if(token.contains("-")){
+					String[] adpo=token.split("-");
+					for (String addr :adpo) {
+						String [] po=adpo[1].toString().split(" ");
+						System.out.println(po[1]);
+						
+						
+					}
+				}
+					
+				
+				
+				
+				//Print all tokens
+					//System.out.println(token);
+				}
+		    }
+		}
+	    catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally	{
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
